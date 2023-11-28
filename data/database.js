579f29d5fe1249121c9e22ab96e7d19fe07b78a5
davidkvoacweb <davidkovacwebdev@gmail.com>
2023-11-28 15:44:29 +0100

    fixed evry

--- data/database.js
+++ data/database.js
@@ -3,14 +3,14 @@ import { MongoClient } from 'mongodb';
 const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
 const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
-const dbName = process.secrets.MONGODB_USERNAME;
+const dbName = process.env.MONGODB_DB_NAME;
 
 const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
 console.log('Trying to connect to db');
 console.log(dbPassword)
-console.log(dbName)
+console.log(dbPassword)
 
 
 try {
